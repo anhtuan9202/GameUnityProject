2f562c588660d15b789bed77ace008532ee6dd6e
anhtuan9202 <123trananhtuan@gmail.com>
2023-05-04 16:06:19 +0700

    update

---
+++ Assets/Script/VolumeController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+
+    public Slider sliderBackground;
+    public Slider sliderGame;
+    public AudioSource audioGame;
+    public AudioSource audioBackground;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void Save(float volume)
+    {
+        
+    }
+}
